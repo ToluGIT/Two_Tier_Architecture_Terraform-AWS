8261c85d28ca753b1cb254d9aaca9d674ca4621f
ToluGIT <93225033+ToluGIT@users.noreply.github.com>
2024-11-19 14:23:28 -0700

    Update providers.tf
    Adding S3 statelock

--- Terra/providers.tf
+++ Terra/providers.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terras3-state"      # Replace with your actual S3 bucket name
+    key            = "terraform/state.tfstate"          # Path to store the state file within the bucket
+    region         = "us-east-1"                        # Replace with your AWS region
+    encrypt        = true                               # Enable server-side encryption
+    dynamodb_table = "terraform-lock-table"             # Replace with your DynamoDB table name for state locking
+  }
+}
